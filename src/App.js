1c0ec49fe801529515dc0a727fdf68859bad89b4
JoynalAbedin <jolobangladesh@gmail.com>
2022-04-18 16:32:49 +0600

    listing implemented

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
@@ -11,6 +11,7 @@ import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
 import SignUp from './pages/SignUp';
 import Category from './pages/Category';
+import CreateListing from './pages/CreateListing.jsx';
 
 function App() {
     return (
@@ -29,6 +30,10 @@ function App() {
                     <Route
                         path="/forgotpassword"
                         element={<ForgotPassword />}
+                    />
+                    <Route
+                        path="/create-listing"
+                        element={<CreateListing />}
                     />
                 </Routes>
                 <Navbar />
